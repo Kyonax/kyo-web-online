6c4cf7d92c506bef317ec46e3fa53457a5cfd55f
Kyonax <kyonax25@gmail.com>
2024-05-20 19:04:10 -0500

    Creating Web Structure

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './dir/scss/index.scss'
+
+import reportWebVitals from './dir/config/reportWebVitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
